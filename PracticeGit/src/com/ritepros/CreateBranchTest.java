59f0b0cdc4badf106903005225744fdef6203281
jinisha2193 <jinisha2193@gmail.com>
2016-02-23 13:05:33 +0530

    partially implemented

---
+++ PracticeGit/src/com/ritepros/CreateBranchTest.java
@@ -0,0 +1,12 @@
+package com.ritepros;
+
+public class CreateBranchTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("joseph");
+		System.out.println("we created a new change");
+
+	}
+
+}
