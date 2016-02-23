6262b7e56147e02a57476a04cbaf261811dc6591
jinisha2193 <jinisha2193@gmail.com>
2016-02-23 13:08:07 +0530

    added tuesday

--- PracticeGit/src/com/ritepros/HelloGit.java
+++ PracticeGit/src/com/ritepros/HelloGit.java
@@ -4,9 +4,9 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello");
+		System.out.println("hello tuesday");
 		System.out.println("its a beautiful day to save lives");
-		System.out.println("hiii");
+		System.out.println("hiii its tuesday");
 		System.out.println("jinisha");
 
 	}
