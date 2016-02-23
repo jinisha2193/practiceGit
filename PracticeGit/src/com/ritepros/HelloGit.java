f3cc76df2a98a2d416690c34a7c65ccd16b08ed6
jinisha2193 <jinisha2193@gmail.com>
2016-02-23 13:17:47 +0530

    called display function

--- PracticeGit/src/com/ritepros/HelloGit.java
+++ PracticeGit/src/com/ritepros/HelloGit.java
@@ -4,6 +4,8 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		CreateBranchTest obj = new CreateBranchTest();
+		obj.display();
 		System.out.println("hello");
 		System.out.println("its a beautiful day to save lives");
 		System.out.println("hiii");
